bca9eab0827322a279124461bed3972fd3913553
Eric Freitas Avelino <128161569+Crire@users.noreply.github.com>
2024-05-21 11:04:46 -0400

    Add files via upload

---
+++ homeadd.php
@@ -0,0 +1,56 @@
+<?php
+
+
+// Create connection
+$conn = new mysqli('localhost', 'root', "", 'atividadeweb');
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$query = "SELECT * FROM Posts";
+$resultado = $conn->query($query);
+
+while ($row = mysqli_fetch_assoc($resultado)) {
+    echo
+        "<div class='div-cabecalho'>" .
+            "<div class='teste'>" .
+                "<img src='./assests/img/icons8-bmo-48.png'>" .
+                "<p id='nome'>" . $row["PosAutor"] ."</p>" .
+            "</div>" .
+            "<button id='categoria'>". $row["PosArea"] . "</button>" .
+            "<a href='#' id='like'><i class='fa-regular fa-heart'></i></a>" .
+        "</div>" .
+        "<div class='div-texto'>" .
+            "<p>" . $row["PosTexto"] . "</p>" .
+        "</div>";
+    
+}
+
+
+if (isset($_POST['add'])) {
+    $titulo = $_POST['titulo-nota'];
+    $categoria = $_POST['categoria-nota'];
+    $conteudo = $_POST['conteudo'];
+    //$add = ;
+    if($categoria != "" && $conteudo != ""){
+        $query = "insert into Posts(PosAutor, PosArea, PosTexto) values(?, ?, ?)";
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param("sss", $titulo, $categoria, $conteudo);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($stmt->affected_rows > 0) {
+            echo "<script>alert('Anotação adicionada com sucesso!'); window.location.href='home.html';</script>";
+        } else {
+            echo "<script>alert('Não foi possível adicionar a anotação'); window.location.href='home.html';</script>";
+        }
+    } else {
+        echo "<script>alert('Todos os campos devem ser preenchidos'); window.location.href='home.html';</script>";
+    }
+}
+
+$conn->close();
+?>
